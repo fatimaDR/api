99b2119d7a8bb2ecc47eac3dd09249027b773ee5
fatima <driouch88.f@gmail.com>
2020-11-09 23:28:53 +0100

    Migration created for Product and Review

--- database/migrations/2020_11_04_165907_create_products_table.php
+++ database/migrations/2020_11_04_165907_create_products_table.php
@@ -15,6 +15,11 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description');
+            $table->float('price', 8, 2);
+            $table->integer('stock');
+            $table->integer('discount');
             $table->timestamps();
         });
     }
